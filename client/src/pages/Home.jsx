8d356e0452fa43040977d2c8d9256285134c709e
ujjawalsingh30 <ujjawalsingh8589@gmail.com>
2026-01-08 18:42:42 +0530

    CarCard section done

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import Hero from '../components/Hero'
+import FeaturedSection from '../components/FeaturedSection'
+import Banner from '../components/Banner'
 
 const Home = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <Hero />
+      <FeaturedSection/>
+      <Banner/>
+    </>
   )
 }
 
